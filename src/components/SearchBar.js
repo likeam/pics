73246b950926785a4b6924db2f71c167b7058c70
AbdulRehman <likeam99@gmail.com>
2022-10-04 01:06:41 +0500

    Building State components

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,18 +2,24 @@ import React, { Component } from 'react'
 
 export class SearchBar extends Component {
 
-    onInputChange(event){
-        console.log(event.target.value);
+    state ={ term: ''};
+
+    onFormSubmit = event =>{
+        event.preventDefault();
+        this.props.onSubmit(this.state.term);
+
     }
 
 
     render() {
         return (
         <div className='ui segment'>
-            <form className='ui form' >
+            <form onSubmit={this.onFormSubmit} className='ui form' >
                 <div className='fied'>
                     <label >Image Search</label>
-                <input type= "text" onChange={this.onInputChange} />
+                <input type= "text"
+                value={this.state.term}
+                onChange={(e) => this.setState({term: e.target.value}) } />
                 </div>
             </form>
         </div>
