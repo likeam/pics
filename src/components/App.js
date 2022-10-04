0501fd597a8345d7623ad85f90f120ae14dc63ff
AbdulRehman <likeam99@gmail.com>
2022-10-05 00:22:01 +0500

    Axios setting

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+
 
 class App extends React.Component {
 
   onSearchSubmit(term){
-    console.log(term);
+    axios.get( 'https://api.unsplash.com/search/photos', {
+      params: {query: term},
+      headers: {
+        Authorization:' Client-ID XvqqYpGQN48PE-Txe2ofCSBxUNIre2_nlSrMt6WeHco'
+      }
+
+    });
   }
 
   render(){
-
 
       return (
         <div className="ui container" style={{marginTop: '10px'}}>
