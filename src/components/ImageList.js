9848516bb88e1eea826f83f547828ac71f26b8fb
AbdulRehman <likeam99@gmail.com>
2022-10-11 04:21:23 +0500

    rendering the image

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,12 +1,16 @@
 import React from 'react'
+import './ImageList.css'
+import ImageCard from './ImageCard';
 
 const ImageList = props => {
+    const images = props.images.map((image) => {
+        return <ImageCard key={image.id} image={image} />;
+            
+    });
 
     return (
-        <div>
-        Image List
-        </div>
+        <div className='image-list' >{images}</div>
     )
 }
 
-export default ImageList
+export default ImageList;
